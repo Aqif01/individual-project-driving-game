b336e40a933714abed27ae3743c25fdd1edd8916
AQIF\Aqif <aqifclash@gmail.com>
2017-04-10 23:16:42 +0100

    Added Pause Button
    Pause button created and fully tested.

---
+++ Driving Game/Assets/UImanager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UImanager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Pause (){
+		if (Time.timeScale == 1) {
+			Time.timeScale = 0;
+		} else if (Time.timeScale == 0) {
+			Time.timeScale = 1;
+		}
+			
+	}
+}
